7be494f06ddeeb7bea7546d92da6187d463413ad
Lucas Fernandez <lucas.emiliano.fernandez@gmail.com>
2021-09-09 21:00:59 -0300

    Entrega clase 3

---
+++ main.js
@@ -0,0 +1,9 @@
+//let numero = parseInt(prompt("Ingrese un numero del dia para saber cuantos meses faltan para terminar septiembre"))
+
+
+
+for(let numero = parseInt(prompt("Ingrese un numero")); numero < 10; numero++){
+    alert(numero + 10)
+}
+
+
